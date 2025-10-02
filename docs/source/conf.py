6b3250ac2afc5bf976a8f2c46dddf2b84308c587
matt-wyatt-e3 <matthew.wyatt@ethree.com>
2025-10-02 15:40:01 -0700

    Trying to revert merge errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,19 +2,15 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import pathlib
+import os
 import sys
 
-from sphinxawesome_theme.postprocess import Icons
-
-print(pathlib.Path(__file__).parents[2] / "tests")
-sys.path.insert(0, str(pathlib.Path(__file__).parents[2]))
+sys.path.insert(0, os.path.abspath("../../new_modeling_toolkit"))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Kit"
-copyright = "2024, Energy & Environmental Economics, Inc."
+project = "Resolve"
+copyright = "2023, Energy & Environmental Economics, Inc."
 author = "Energy & Environmental Economics, Inc."
 
 
@@ -33,78 +29,57 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
+    "sphix_copybutton",
     "sphinxcontrib.autodoc_pydantic",
     "sphinx_design",
     "sphinx_last_updated_by_git",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["../_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 # exclude_patterns = []
 
-# Intentionally not inheriting all autodoc members, because I want to be able to control what pydantic model
-# attributes are shown (e.g., not attrs inherited from`Component` or `CustomModel`)
-autodoc_default_options = {
-    "inherited-members": False,
-}
-
-autodoc_pydantic_field_list_validators = False
-autodoc_pydantic_model_show_config_member = False
-autodoc_pydantic_model_show_config_summary = False
-autodoc_pydantic_model_show_json = False
-autodoc_pydantic_model_show_validator_members = False
-autodoc_pydantic_model_show_validator_summary = False
-autodoc_pydantic_model_signature_prefix = "class"
-
 add_module_names = False
 toc_object_entries_show_parents = "hide"
 
-intersphinx_mapping = {"pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None)}
-myst_url_schemes = ["https"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinxawesome_theme"
-html_favicon = "e3-logo.ico"
+html_theme = "furo"
+html_favicon = "_images/e3-logo.ico"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["css/e3.css"]
+html_css_files = ["css/furo-e3.css"]
 
 html_title = "RESOLVE"
 
-html_sidebars = {
-    "branding": ["sidebar_main_nav_links.html"],
-    "dev": ["sidebar_main_nav_links.html"],
-    "roadmap": ["sidebar_main_nav_links.html"],
-}
-
 html_theme_options = {
-    "awesome_external_links": True,
-    "breadcrumbs_separator": Icons.chevron_right,
-    "logo_dark": "_static/logos/kit-dark.svg",
-    "logo_light": "_static/logos/kit-light.svg",
-    "main_nav_links": {
-        "Branding": "branding",
-        "Development Guide": "dev",
-        "Changelog & Roadmap": "roadmap",
+    "light_css_variables": {
+        "color-brand-primary": "#034E6E",  # The standard E3 dark blue
+        "color-brand-background": "#E2ECF0",  # The E3 light blue/grey background color
+        "color-brand-dropdown": "white",
     },
-    "show_breadcrumbs": True,
-    "show_prev_next": True,
-    "show_scrolltop": True,
+    "light_logo": "resolve-logo-light.svg",
+    "dark_css_variables": {
+        "color-brand-primary": "#C4AD73",
+        "color-brand-background": "#212529",  # Same as sphinx-design --sd-color-dark
+        "color-brand-dropdown": "#212529",  # Same as sphinx-design --sd-color-dark
+    },
+    "dark_logo": "resolve-logo-dark.svg",
+    "sidebar_hide_name": True,
+    "top_of_page_button": "edit",
 }
-
-graphviz_output_format = "svg"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -134,12 +109,10 @@ mermaid_d3_zoom = True
 """
 1. If you need to update the autodoc files (e.g., structure changes),
    run the following command from the docs/ directory
-    sphinx-apidoc -f -o source [absolute path to new_modeling_toolkit directory]
-2. Convert the .rst files to .md using the command rst2myst 
-   (https://docs.readthedocs.io/en/stable/guides/migrate-rest-myst.html#how-to-convert-existing-restructuredtext-documentation-to-myst)
-2. Add text to the .md files manually for more documentation
-3. Run `make html` to update the HTML files
-4. Run `make latex` to update LaTeX files
+    sphinx-apidoc -f -o source [absolute path to src directory]
+2. Add text to the RST files manually for more documentation
+3. Run make html to update the HTML files
+4. Run make latex to update LaTeX files
 
 https://ethreesf.sharepoint.com/sites/Training/_layouts/OneNote.aspx?id=%2Fsites%2FTraining%2FSiteAssets%2FTraining%20Notebook&wd=target%28Technical%20Skills%20Training%2FPython.one%7C916C3A04-A4B3-4112-9E7F-F2F503E5B87C%2FDocumentation%20%26%20Docstrings%7C72764774-57EE-4BC6-8496-E63524712FF6%2F%29
 """
